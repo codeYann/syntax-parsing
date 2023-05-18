2d1ea068ebd2ca7a02343b49a6243c04f0886183
Yan Rodrigues <ccyan05@alu.ufc.br>
2023-05-18 15:56:31 -0300

    Main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
+from lexer.code_handler import CodeHandler
+from lexer.lexer import Lexer
+import asyncio
+
+
+async def main():
+    handler = CodeHandler()
+    source_code = await handler.read_code_file("program.xqdl")
+
+    lexer = Lexer()
+    try:
+        tokens = lexer.tokenize(source_code)
+        for token in tokens:
+            print(token.__str__())
+    except ValueError as e:
+        print(e)
+
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    asyncio.run(main())
